faac5a082b9b3a137c68d527d8a124ba37a53ef7
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-11-08 19:12:21 +0100

    Build only for python 2.7

--- build.py
+++ build.py
@@ -32,5 +32,6 @@ def set_properties(project):
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        'Programming Language :: Python2.7',
         'Topic :: System :: Systems Administration'
     ])
